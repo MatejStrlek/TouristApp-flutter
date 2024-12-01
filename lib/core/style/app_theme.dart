b71a708871f28ffd626f93588e8d649422281903
Matej Galic <matej.galic.ets@gmail.com>
2024-12-01 16:04:51 +0100

    location card design done

--- lib/core/style/app_theme.dart
+++ lib/core/style/app_theme.dart
@@ -13,6 +13,7 @@ final lightTheme = ThemeData(
       error: errorColorLight,
       gradientBegin: gradientBeginColorLight,
       gradientEnd: gradientEndColorLight,
+      cardText: cardTextColorLight,
     ),
   ],
 );
--- lib/features/locations/presentation/location_list/screen/location_list_screen.dart
+++ lib/features/locations/presentation/location_list/screen/location_list_screen.dart
@@ -3,6 +3,7 @@ import 'package:faks/core/style/style_extensions.dart';
 import 'package:faks/features/locations/presentation/location_list/controller/state/location_list_state.dart';
 import 'package:faks/features/locations/presentation/location_list/widget/empty_state_widget.dart';
 import 'package:faks/features/locations/presentation/location_list/widget/error_state_widget.dart';
+import 'package:faks/features/locations/presentation/location_list/widget/location_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lottie/lottie.dart';
@@ -21,17 +22,22 @@ class LocationListScreen extends ConsumerWidget {
         children: [
           Text('Places', style: context.textTitle),
           switch (state) {
-            LoadingState() => Expanded(child: Center(child: Lottie.asset("assets/animations/loading_sights.json", width: 100),)),
+            LoadingState() => Expanded(
+                  child: Center(
+                child: Lottie.asset("assets/animations/loading_sights.json",
+                    width: 100),
+              )),
             EmptyState() => const Expanded(child: EmptyStateWidget()),
-            ErrorState(failure: var failure) => Expanded(child: ErrorStateWidget(failure: failure)),
+            ErrorState(failure: var failure) =>
+              Expanded(child: ErrorStateWidget(failure: failure)),
             FilledState(locations: var list) => Expanded(
-              child: ListView.separated(
+                child: ListView.separated(
                   itemCount: list.length,
                   separatorBuilder: (_, __) => const SizedBox(height: 10),
                   itemBuilder: (context, index) =>
-                      Card(child: Text(list[index].title)),
+                      LocationCard(list[index]),
                 ),
-            )
+              )
           }
         ],
       ),
